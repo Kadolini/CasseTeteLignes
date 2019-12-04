71139992ecbe299268d8dea0b2893c04ba145429
FazulNazar <43197941+FazulNazar@users.noreply.github.com>
2019-12-04 15:26:39 +0100

    startGame return

--- CasseTete/src/Modele/Game.java
+++ CasseTete/src/Modele/Game.java
@@ -23,19 +23,21 @@ public class Game{
 
     }
     
-    public void startGame(){
+    public int startGame(){
         board.initBoard(parser.getBoardSize()[0],parser.getBoardSize()[0]);
         while(!board.getBoardIsFull() && !board.getSymbolsLinked()){
             // on fait tourner le jeu ici
             if(board.getBoardIsFull() && !board.getSymbolsLinked()){
                 System.out.println("Oups! Les chemins ne sont pas bons.");
+                return 0;
             }
             if(!board.getBoardIsFull() && board.getSymbolsLinked()){
                 System.out.println("Oups! Vous n'avez pas utiliser toutes les cases.");
+                return 0;
             }
         }
         System.out.println("Félicitations vous avez résolu le puzzle.");
-        
+        return 1;
     }
 
     public Board getBoard() {
